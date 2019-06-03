d4d200204a19b0d1d8cb19c674e56640081d9bb2
ayush salik <asalik13@gmail.com>
2019-06-03 16:35:42 +0530

    Added the daily data function

---
+++ the_fame_project.py
@@ -0,0 +1,17 @@
+
+import requests
+import datetime
+
+
+def dailycount(Name):
+    dt = datetime.datetime.now()
+    today=(dt - datetime.timedelta(2)).strftime('%Y%m%d')+"00"
+    Name=Name.replace(" ","_")
+
+    URL = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{}/daily/{}/{}".format(Name,today,today)
+    r = requests.get(url = URL)
+    data = r.json()
+    return(data['items'][0]['views'])
+
+
+print(dailycount("Vitaly Zdorovetskiy"))
