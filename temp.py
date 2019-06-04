06f5c3c1b881127cf43aa43897e299c96c398fa3
ayush salik <asalik13@gmail.com>
2019-06-05 00:32:33 +0530

    Spell check and csv database

---
+++ temp.py
@@ -0,0 +1,43 @@
+import difflib
+import csv
+import requests
+import datetime
+
+
+
+
+mylist=[]
+with open("myfile.csv") as fd:
+    rd = csv.reader(fd)
+    for row in rd:
+        for a in row:
+            mylist.append(a)
+
+
+
+
+def is_similar(first, second, ratio):
+    return difflib.SequenceMatcher(None, first, second).ratio() > ratio
+
+
+input = ['aleander fleming']
+
+result = [s for f in input for s in mylist if is_similar(f,s, 0.8)]
+
+
+
+def dailycount(Name):
+    dt = datetime.datetime.now()
+    today=(dt - datetime.timedelta(2)).strftime('%Y%m%d')+"00"
+    Name=Name.replace(" ","_")
+
+    URL = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{}/daily/{}/{}".format(Name,today,today)
+    r = requests.get(url = URL)
+    data = r.json()
+    return(data['items'][0]['views'])
+
+dailycount(result[0])
+
+"""with open('myfile.csv', 'w') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(mylist)"""
--- the_fame_project.py
+++ the_fame_project.py
@@ -5,7 +5,7 @@ import datetime
 
 def dailycount(Name):
     dt = datetime.datetime.now()
-    today=(dt - datetime.timedelta(1)).strftime('%Y%m%d')+"00"
+    today=(dt - datetime.timedelta(11)).strftime('%Y%m%d')+"00"
     Name=Name.replace(" ","_")
 
     URL = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{}/daily/{}/{}".format(Name,today,today)
@@ -14,4 +14,4 @@ def dailycount(Name):
     return(data['items'][0]['views'])
 
 
-print(dailycount("Narendra Modi"))
+print(dailycount("Bastille_(band)"))
